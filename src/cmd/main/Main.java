2a71a3e869ac2c65223eaf66986645c75ab1f75d
beintil <sergeisavilov4@gmail.com>
2023-02-17 20:07:54 +0700

    class main

---
+++ src/cmd/main/Main.java
@@ -0,0 +1,10 @@
+package cmd.main;
+
+import internal.transport.Automobile;
+
+public class Main {
+    public static void main(String[] args) {
+        Automobile bmw = new Automobile("black", 157.97f, 1500, "bmw", 2015);
+        System.out.println(bmw.info());
+    }
+}
