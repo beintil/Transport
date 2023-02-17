d63d25e85ebe2745edc6d8a251e000cd58c4d6e9
beintil <sergeisavilov4@gmail.com>
2023-02-17 20:18:03 +0700

    main file

--- src/cmd/main/Main.java
+++ src/cmd/main/Main.java
@@ -5,6 +5,6 @@ import internal.transport.Automobile;
 public class Main {
     public static void main(String[] args) {
         Automobile bmw = new Automobile("black", 157.97f, 1500, "bmw", 2015);
-        System.out.println(bmw.info());
+        System.out.println(bmw.get.view());
     }
 }
